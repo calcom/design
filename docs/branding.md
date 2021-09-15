4d72888d1441fa1f8001f3f48636ff3217c30bf4
Peer Richelsen <peeroke@richelsen.net>
2021-09-15 14:23:00 +0100

    added branding section to design docs

---
+++ docs/branding.md
@@ -0,0 +1,30 @@
+# Branding
+
+We use our own **Cal Sans** typography, made by [Mark Davis](https://twitter.com/MarkFonts) for our logo and every headline.
+
+Download our Cal.com logo below:
+
+
+## Logos
+
+<div style={{background: "#292929", display: "block", padding: "48px", textAlign: "center", marginBottom: 16}}>
+<img src="https://cal.com/logo-white.svg" />
+<br/>
+<a download href="https://cal.com/logo-white.svg" title="Cal.com Logo White">
+    Download
+</a>
+</div>
+
+
+<div style={{background: "#ffffff", display: "block", padding: "48px", textAlign: "center", marginBottom: 16}}>
+<img src="https://cal.com/logo.svg" /><br/>
+<a download href="https://cal.com/logo.svg" title="Cal.com Logo White">
+    Download
+</a>
+</div>
+
+## Brand colors
+
+| White                   | Off White    | Black                  |    
+| ---------------------- | ----------- | -------------------------- | 
+| #FFFFFF            | #F5F5F5     | #292929 |
--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -4,36 +4,38 @@ import styles from './HomepageFeatures.module.css';
 import Link from '@docusaurus/Link';
 
 const CategoryList = [
-    {
-        link: '/docs/colors',
-        title: 'Colors',
-        Svg: require('../../static/img/categories/colors.svg').default,
-        description: (
-            <>
-                Our color palette that all of our applications and services use.
-            </>
-        ),
-    },
-    {
-        link: '/docs/typography',
-        title: 'Typography',
-        Svg: require('../../static/img/categories/typography.svg').default,
-        description: (
-            <>
-                The fonts that we use and the best practices surrounding text.
-            </>
-        ),
-    },
-    {
-        link: '/docs/icons',
-        title: 'Icons',
-        Svg: require('../../static/img/categories/icons.svg').default,
-        description: (
-            <>
-                Our icon set and guidelines on when to use each type of icon.
-            </>
-        ),
-    },
+  {
+    link: "/docs/branding",
+    title: "Cal Branding",
+    Svg: require("../../static/img/categories/default.svg").default,
+    description: (
+      <>Our branding guidelines and *.svgs to download.</>
+    ),
+  },
+  {
+    link: "/docs/colors",
+    title: "Colors",
+    Svg: require("../../static/img/categories/colors.svg").default,
+    description: (
+      <>Our color palette that all of our applications and services use.</>
+    ),
+  },
+  {
+    link: "/docs/typography",
+    title: "Typography",
+    Svg: require("../../static/img/categories/typography.svg").default,
+    description: (
+      <>The fonts that we use and the best practices surrounding text.</>
+    ),
+  },
+  {
+    link: "/docs/icons",
+    title: "Icons",
+    Svg: require("../../static/img/categories/icons.svg").default,
+    description: (
+      <>Our icon set and guidelines on when to use each type of icon.</>
+    ),
+  },
 ];
 
 function Category({Svg, title, description, link}) {
